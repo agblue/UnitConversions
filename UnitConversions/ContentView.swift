5346bad0a5c99efbb34e7ccaf2d679347f320530
Danny Tsang <danny.tsang@me.com>
2023-09-06 13:32:42 -0500

    Added Unit Conversion for Time based units.

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -7,15 +7,99 @@
 
 import SwiftUI
 
+enum TimeUnit: String {
+    case seconds = "Seconds"
+    case minutes = "Minutes"
+    case hours = "Hours"
+    case days = "Days"
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var inputUnit: TimeUnit = .seconds
+    @State private var outputUnit: TimeUnit = .seconds
+    @State private var inputAmount = 0.00
+
+    @FocusState private var isAmountFocused: Bool
+
+    private var outputAmount: Double {
+        var inputBase = Double(inputAmount)
+        switch inputUnit {
+        case .seconds:
+            break
+        case .minutes:
+            inputBase = inputBase * 60
+        case .hours:
+            inputBase = inputBase * 60 * 60
+        case .days:
+            inputBase = inputBase * 24 * 60 * 60
         }
-        .padding()
+
+        var outputBase = inputBase
+        switch outputUnit {
+        case .seconds:
+            break
+        case .minutes:
+            outputBase = outputBase / 60
+        case .hours:
+            outputBase = outputBase / 60 / 60
+        case .days:
+            outputBase = outputBase / 24 / 60 / 60
+        }
+
+        return outputBase
+    }
+
+    let timeUnits: [TimeUnit] = [.seconds, .minutes, .hours, .days]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text("\($0.rawValue)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Input Unit")
+                }
+
+                Section {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text("\($0.rawValue)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Output Unit")
+                }
+
+                Section {
+                    TextField("Input Amount", value: $inputAmount, format: .number)
+                        .keyboardType(.numberPad)
+                        .focused($isAmountFocused)
+                } header: {
+                    Text("Input \(inputUnit.rawValue)")
+                }
+
+                Section {
+                    Text("\(outputAmount, format: .number) \(outputUnit.rawValue)")
+                } header: {
+                    Text("Output Result")
+                }
+            }
+            .navigationTitle("Unit Conversions")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isAmountFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
